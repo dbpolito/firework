f7f0c7df762a0220add95a17feccea0afad331a3
Daniel Polito <contato@dbpolito.net>
2012-09-12 18:52:28 -0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+
+require __DIR__.'/src/Firework/Firework.php';
+
+use \Firework\Benchmark;
+
+Benchmark::start('Starting Benchmark');
+
+    Benchmark::start('For\'s Benchmark');
+
+        Benchmark::start('For 1');
+        for ($i=1;$i<10000;$i++) {
+            // Do something
+        }
+        Benchmark::end();
+
+        Benchmark::start('For 2');
+        for ($i=1;$i<10000;++$i) {
+            // Do something
+        }
+        Benchmark::end();
+
+    Benchmark::end();
+
+    Benchmark::start('While\'s Benchmark');
+
+        Benchmark::start('While 1');
+        $i = 1;
+        while ($i<10000) {
+            $i++;
+        }
+        Benchmark::end();
+
+        Benchmark::start('While 2');
+        $i = 1;
+        while ($i<10000) {
+            ++$i;
+        }
+        Benchmark::end();
+
+    Benchmark::end();
+
+Benchmark::end();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Firework - Benchmark</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Le styles -->
+<link href="http://rc.getbootstrap.com/assets/css/bootstrap.css" rel="stylesheet">
+<link href="http://rc.getbootstrap.com/assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<!-- Le fav and touch icons -->
+<link rel="shortcut icon" href="assets/ico/favicon.ico">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://rc.getbootstrap.com/assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://rc.getbootstrap.com/assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://rc.getbootstrap.com/assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="http://rc.getbootstrap.com/assets/ico/apple-touch-icon-57-precomposed.png">
+
+</head>
+
+<body>
+<div class="container">
+    <div class="hero-unit">
+        <h1>Firework - Benchmark</h1>
+        <p>A powerfull component that helps you benchmark your code easily</p>
+        <a class="btn btn-primary btn-large">Learn more</a>
+    </div>
+    <table class="table table-bordered table-striped table-hover">
+        <thead>
+            <tr>
+                <th>Label</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th>Elapsed Time</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php Benchmark::toHTML(); ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
---
+++ src/Firework/Firework.php
@@ -0,0 +1,117 @@
+<?php
+namespace Firework;
+
+class Benchmark
+{
+
+    protected static $marks = array();
+
+    protected static $index = array();
+
+    protected static $active = null;
+
+    public static function start($label = null)
+    {
+        if (static::$active === null)
+        {
+            $key = '0';
+        }
+        else
+        {
+            $key = static::$active.'.marks';
+            if (($keys = static::getArrayValue(static::$marks, $key)) !== null)
+            {
+                $keys = array_keys($keys);
+                $key .= '.'.count($keys);
+            }
+            else
+            {
+                $key .= '.0';
+            }
+        }
+
+        static::$active = $key;
+
+        $mark = array(
+            'label' => $label,
+            'start' => static::getTime(),
+            'end'   => null,
+            'marks' => null
+        );
+
+        static::$index[$key] = $label;
+
+        static::setArrayValue(static::$marks, $key, $mark);
+    }
+
+    public static function end()
+    {
+        if (static::$active === null) {
+            throw new \Exception('There is no more marks to end.');
+        }
+
+        static::setArrayValue(static::$marks, static::$active.'.end', static::getTime());
+
+        $active = explode('.', static::$active);
+        array_pop($active);
+        array_pop($active);
+
+        static::$active = implode('.', $active);
+    }
+
+    protected static function getTime()
+    {
+        return microtime(true);
+    }
+
+    protected static function getArrayValue($array, $key)
+    {
+        if (! is_array($array) and ! $array instanceof \ArrayAccess) {
+            throw new \InvalidArgumentException('First parameter must be an array or ArrayAccess object.');
+        }
+
+        foreach (explode('.', $key) as $_key) {
+            if (isset($array[$_key]) === false) {
+                return null;
+            }
+
+            $array = $array[$_key];
+        }
+
+        return $array;
+    }
+
+    protected static function setArrayValue(&$array, $key, $value = null)
+    {
+        if ( ! is_array($array) and ! $array instanceof \ArrayAccess) {
+            throw new \InvalidArgumentException('First parameter must be an array or ArrayAccess object.');
+        }
+
+        foreach (explode('.', $key) as $_key) {
+            if (isset($array[$_key]) === false) {
+                $array[$_key] = array();
+            }
+
+            $array =& $array[$_key];
+        }
+
+        $array = $value;
+    }
+
+    public static function toHtml()
+    {
+        foreach (static::$index as $key => $velue)
+        {
+            $indent = substr_count($key, '.');
+            $mark = static::getArrayValue(static::$marks, $key);
+        ?>
+        <tr>
+            <td style="text-indent:<?php echo $indent*15; ?>px"><?php echo $mark['label']; ?></td>
+            <td><?php echo $mark['start']; ?></td>
+            <td><?php echo $mark['end']; ?></td>
+            <td><?php echo number_format($mark['end']-$mark['start'], 4); ?>µs</td>
+        </tr>
+        <?php
+        }
+    }
+}
